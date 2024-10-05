f9f0d06507f48ab422f0f7e84131d7d4e58cc1f0
sunilaadhe <sunilaadhe62@gmail.com>
2024-10-05 19:51:00 +0530

    one

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-
-const input  = document.querySelector("#input");
 const btn  = document.querySelector("button");
 const one  = document.querySelector(".one");
 const two  = document.querySelector(".two");
 
-const getReceipe = async() => {
+const joke = async() => {
 
  
 
     const responce = await fetch(`https://official-joke-api.appspot.com/random_joke`)
+   
     const data = await responce.json();
     console.log(data)
-    setTimeout(()=>{
+
     console.log(data.setup)
     console.log(data.punchline)
-},2000)
+
     one.innerHTML = data.setup;
     two.innerHTML = data.punchline;
 
 }
 
 btn.addEventListener("click",(e) => {
-    getReceipe();
+    joke();
+
 })
